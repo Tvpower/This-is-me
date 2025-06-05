b241d331e1ad83f76045ecd20eaf5cab20d8254a
tvpower <tvpower89@gmail.com>
2025-06-05 12:00:48 -0700

    added eye favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/red-eye.png" />
+      </head>
       <body>{children}</body>
     </html>
   )
